45c74c1aeb89c93fa1b0dc74850304d68ad5f925
phuongdh <phuongdh@evolableasia.vn>
2018-10-05 10:50:52 +0700

    add adamlambert.c

---
+++ adamlambert.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("What's your name?\n");
+    printf("It's Adam. Adam Lambert."\n);
+
+    int a = 3;
+    int b = 10;
+
+    printf("%d + %d = %d", a, b, a+b);
+
+    return 0;
+}
